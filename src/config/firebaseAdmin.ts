9089e15c8e00e26a73c1ccfd83931add6c5fddbe
Razib987 <mohammadrazib987@gmail.com>
2026-02-22 02:27:57 +0600

    final try

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -1,8 +1,11 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../serviceAccountKey.json";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
-});
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export default admin;
