d57f1b330644b6e38f8fcd23e3b46f331e40f2ce
Razib987 <mohammadrazib987@gmail.com>
2026-02-23 14:15:11 +0600

    create me fixed

--- src/app.ts
+++ src/app.ts
@@ -58,7 +58,7 @@ app.use(
 app.options(
   "*",
   cors({
-    origin: "https://safarapp.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
