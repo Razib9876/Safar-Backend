822c8948980790fb999cd615572636fb5d030896
Razib987 <mohammadrazib987@gmail.com>
2026-02-23 18:07:26 +0600

    last try

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -1,9 +1,11 @@
 import admin from "firebase-admin";
 
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  projectId: process.env.FIREBASE_PROJECT_ID as string,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
 };
 
 if (!admin.apps.length) {
